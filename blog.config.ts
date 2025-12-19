259bcfb2ba34eb37574f7d92ac49eb3ee491afd5
abloom25 <evereen2023@gmail.com>
2025-12-19 14:48:32 +0800

    更新博客统计

--- blog.config.ts
+++ blog.config.ts
@@ -70,8 +70,8 @@ const blogConfig = {
 	/** 向 <head> 中添加脚本 */
 	scripts: [
 		// TODO
-		// // 自己部署的 Umami 统计服务
-		// { 'src': 'https://zhi.zhilu.cyou/zhi.js', 'data-website-id': 'a1997c81-a42b-46f6-8d1d-8fbd67a8ef41', 'defer': true },
+		// 自己部署的 Umami 统计服务
+		{ 'src': 'https://cloud.umami.is/script.js', 'data-website-id': 'edb4a9d1-6dd6-434f-813f-cec3e89338fc', 'defer': true },
 		// // 自己网站的 Cloudflare Insights 统计服务
 		// { 'src': 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "97a4fe32ed8240ac8284e9bffaf03962"}', 'defer': true },
 		// Twikoo 评论系统
